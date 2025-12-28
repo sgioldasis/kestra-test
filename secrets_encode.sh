f6669439e7f10a0bd665393969e83b423f27958b
Savas Gioldasis <s.gioldasis-si@kaizengaming.com>
2025-12-28 07:50:01 +0200

    update secrets_encode.sh

--- secrets_encode.sh
+++ secrets_encode.sh
@@ -1,3 +1,7 @@
-while IFS='=' read -r key value; do    
-    echo "SECRET_$key=$(echo -n "$value" | base64)";
+#!/bin/bash
+while IFS='=' read -r key value; do
+    # Skip empty lines and comments
+    [[ -z "$key" || "$key" =~ ^# ]] && continue
+    encoded_value=$(printf "%s" "$value" | base64 | tr -d '\n')
+    echo "SECRET_$key=$encoded_value";
 done < .env > .env_encoded
